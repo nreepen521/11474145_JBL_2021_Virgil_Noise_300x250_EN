46f8fa231b3a95fa2a0701526105e47c332c8b20
nreepen521 <94923380+nreepen521@users.noreply.github.com>
2021-11-23 23:29:14 +0600

    html

---
+++ main.js
@@ -0,0 +1,43 @@
+// Background
+TweenMax.to('.first',1,{autoAlpha:1,delay:1});
+TweenMax.to('.first',1,{autoAlpha:0,delay:5});
+
+TweenMax.to('.second',0,{autoAlpha:0});
+TweenMax.to('.second',1,{autoAlpha:1,delay:5});
+TweenMax.to('.second',1,{autoAlpha:0,delay:10});
+
+TweenMax.to('.third',0,{autoAlpha:0});
+TweenMax.to('.third',1,{autoAlpha:1,delay:10});
+TweenMax.to('.third',1,{autoAlpha:0,delay:15});
+
+TweenMax.to('.fourth',0,{autoAlpha:0});
+TweenMax.to('.fourth',1,{autoAlpha:1,delay:15});
+
+// Content
+TweenMax.to('.logo',0,{autoAlpha:0,});
+TweenMax.to('.logo',1,{autoAlpha:1, delay:1});
+
+TweenMax.to('.man',0,{autoAlpha:0,});
+TweenMax.to('.man',1,{autoAlpha:1, delay:2});
+
+TweenMax.to('.heading',0,{autoAlpha:0,});
+TweenMax.to('.heading',1,{autoAlpha:1, delay:3,});
+
+TweenMax.to('.heading2',0,{autoAlpha:0,});
+TweenMax.to('.heading2',1,{autoAlpha:1, delay:7,});
+
+TweenMax.to('.heading3',0,{autoAlpha:0,});
+TweenMax.to('.heading3',1,{autoAlpha:1, delay:11,});
+
+TweenMax.to('.heading4',0,{autoAlpha:0,});
+TweenMax.to('.heading4',1,{autoAlpha:1, delay:16,});
+
+TweenMax.to('.product',0,{autoAlpha:0,});
+TweenMax.to('.product',1,{autoAlpha:1, delay:6,});
+
+TweenMax.to('.logo4',0,{autoAlpha:0,});
+TweenMax.to('.logo4',1,{autoAlpha:1, delay:17,});
+
+TweenMax.to('.cta',0,{autoAlpha:0,});
+TweenMax.to('.cta',1,{autoAlpha:1, delay:12,});
+
